53937818fa4679d34927e225bfb8af95dfc60b11
kvitee <kvitee41@gmail.com>
2024-07-28 15:54:05 +0000

    Create Task model.

---
+++ prisma/migrations/20240728155053_create_tasks/migration.sql
@@ -0,0 +1,16 @@
+-- CreateTable
+CREATE TABLE "tasks" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "number" INTEGER NOT NULL,
+    "column_id" INTEGER NOT NULL,
+
+    CONSTRAINT "tasks_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "tasks_number_key" ON "tasks"("number");
+
+-- AddForeignKey
+ALTER TABLE "tasks" ADD CONSTRAINT "tasks_column_id_fkey" FOREIGN KEY ("column_id") REFERENCES "columns"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
