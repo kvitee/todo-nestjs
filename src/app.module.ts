0ca71060bcfdf63eae046a0423734a21d1061d5f
kvitee <kvitee41@gmail.com>
2024-07-28 11:09:17 +0000

    Inject PrismaService into UsersService.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,11 @@
 import { Module } from "@nestjs/common";
 
 import { UsersModule } from "./users/users.module";
-import { UsersController } from "./users/users.controller";
-import { UsersService } from "./users/users.service";
+
 
 @Module({
-  controllers: [UsersController],
-  providers: [UsersService],
-  imports: [UsersModule],
+  imports: [
+    UsersModule,
+  ],
 })
 export class AppModule {}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,13 +1,13 @@
 import { Injectable } from "@nestjs/common";
 
+import { PrismaService } from "../prisma/prisma.service";
+
+
 @Injectable()
 export class UsersService {
+  constructor(private prismaService: PrismaService) {}
+
   async getUsers() {
-    return [
-      {
-        id: 1,
-        name: "John",
-      },
-    ];
+    return await this.prismaService.user.findMany();
   }
 }
