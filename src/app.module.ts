ea4b418ea08f3c7149210a27b805e97a16657617
kvitee <kvitee41@gmail.com>
2024-07-29 03:07:24 +0000

    Add config module which loads environment variables.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
 
 import { UsersModule } from "./users/users.module";
 
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: ".env"
+    }),
     UsersModule,
   ],
 })
