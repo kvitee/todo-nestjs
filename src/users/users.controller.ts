0ec1d89d08894434b5a79c0e3fba2bc1427bc750
kvitee <kvitee41@gmail.com>
2024-07-30 06:26:57 +0000

    Change response status codes for PUT and DELETE.

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,5 +1,5 @@
 import {
-  Body, Controller, Delete, Get, Param, Post, Put
+  Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, Post, Put
 } from "@nestjs/common";
 
 import { UsersService } from "./users.service";
@@ -27,11 +27,13 @@ export class UsersController {
   }
 
   @Put("/:id")
+  @HttpCode(HttpStatus.NO_CONTENT)
   updateUser(@Param("id") id: string, @Body() userDto: UpdateUserDto) {
     return this.usersService.update(+id, userDto);
   }
 
   @Delete("/:id")
+  @HttpCode(HttpStatus.NO_CONTENT)
   deleteUser(@Param("id") id: string) {
     return this.usersService.delete(+id);
   }
